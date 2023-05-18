f278f0214a620fe875db9b0572fc2eeb143bdb3c
Marina <95050756+Marindala@users.noreply.github.com>
2023-05-18 18:16:47 -0300

    create data.jsx prub

---
+++ src/data.jsx
@@ -0,0 +1,35 @@
+import img1 from "./assets/img1.jpg";
+import img2 from "./assets/img2.jpg";
+import img3 from "./assets/img3.jpg";
+import img4 from "./assets/img4.jpg";
+import img5 from "./assets/img5.jpg";
+
+const mandalas = [
+  {
+    id: 1,
+    img: img1,
+    alt: "Iamge 1",
+  },
+  {
+    id: 2,
+    img: img2,
+    alt: "Image 2",
+  },
+  {
+    id: 3,
+    img: img3,
+    alt: "Image 3",
+  },
+  {
+    id: 4,
+    img: img4,
+    alt: "Image 4",
+  },
+  {
+    id: 5,
+    img: img5,
+    alt: "Image 5",
+  },
+];
+
+export default mandalas;
