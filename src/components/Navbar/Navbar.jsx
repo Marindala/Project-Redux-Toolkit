fc8bbd7c10d01ea5f30ceec08cf7583e555bb28d
Marina <95050756+Marindala@users.noreply.github.com>
2023-05-19 14:09:26 -0300

    modif css navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,6 +4,8 @@ export const Navbar = () => {
   return (
     <nav className="navbar navbar-dark bg-dark">
     <div className="container">
+      <br></br>
+      <br></br>
       <a className="navbar-brand" href="/">Redux App</a>
     </div>
   </nav>
