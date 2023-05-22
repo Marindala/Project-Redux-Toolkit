2453817c45c83b76ad06411261a1f877a6d4b365
Marina <95050756+Marindala@users.noreply.github.com>
2023-05-22 01:18:57 -0300

    change version react and react-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from 'react-dom/client'
 import App from "./App.jsx";
 //----import bootstrap en Index o Main-------///////
 import "bootstrap/dist/css/bootstrap.css";
